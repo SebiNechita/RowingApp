41b2f6221701572b14c1bc5dfcf53e5102a9ba91
Iannis de Zwart <I.R.E.deZwart@student.tudelft.nl>
2022-12-08 21:32:16 +0100

    Add gateway microservice

---
+++ gateway-microservice/src/main/java/nl/tudelft/sem/template/gateway/controllers/GatewayController.java
@@ -0,0 +1,20 @@
+package nl.tudelft.sem.template.gateway.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello World example controller.
+ * <p>
+ * This controller shows how you can extract information from the JWT token.
+ * </p>
+ */
+@RestController
+public class GatewayController {
+
+    /**
+     * Instantiates a new GatewayController.
+     */
+    @Autowired
+    public GatewayController() {}
+}
