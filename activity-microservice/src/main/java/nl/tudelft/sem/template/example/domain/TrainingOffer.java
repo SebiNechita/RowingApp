f64a2ccf68ab8f0ad3726ac43480907b1520d3d3
Maksym Ziemlewski <M.Ziemlewski@student.tudelft.nl>
2022-12-08 19:52:00 +0100

    Created TrainingOffer class

---
+++ activity-microservice/src/main/java/nl/tudelft/sem/template/example/domain/TrainingOffer.java
@@ -0,0 +1,10 @@
+package nl.tudelft.sem.template.example.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+
+@AllArgsConstructor
+public class TrainingOffer extends ActivityOffer{
+
+}
