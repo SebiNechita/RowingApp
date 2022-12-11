042be6ba2fbdef8d1dc2f06b9efc67db2db03651
Maksym Ziemlewski <M.Ziemlewski@student.tudelft.nl>
2022-12-11 01:37:15 +0100

    Tested TypeOfActivityConverter
    Achieved 100% class coverage in domain

---
+++ activity-microservice/src/test/java/nl/tudelft/sem/template/activity/domain/TypeOfActivityConverterTest.java
@@ -0,0 +1,36 @@
+package nl.tudelft.sem.template.activity.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TypeOfActivityConverterTest {
+
+
+    private TypesOfActivities type;
+    private TypeOfActivityConverter converter;
+
+    @BeforeEach
+    void setup(){
+        this.converter = new TypeOfActivityConverter();
+        this.type = TypesOfActivities.TRAINING;
+    }
+
+    @Test
+    void convertToDatabaseColumn() {
+        assertThat(converter.convertToDatabaseColumn(type)).isEqualTo("TRAINING");
+    }
+
+    @Test
+    void convertToEntityAttribute() {
+        assertThat(converter.convertToEntityAttribute("training")).isEqualTo(type);
+    }
+
+    @Test
+    void convertToEntityAttribute_throwsException() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            converter.convertToEntityAttribute(null);
+        });
+    }
+}
