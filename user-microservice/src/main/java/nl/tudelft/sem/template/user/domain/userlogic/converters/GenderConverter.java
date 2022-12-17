5cad39b5253864d6dd6d98b521e18d910ec7681a
Mihai Apolzan <M.Apolzan@student.tudelft.nl>
2022-12-17 16:41:43 +0100

    Add Gender Converter#2

--- user-microservice/src/main/java/nl/tudelft/sem/template/user/domain/userlogic/converters/GenderConverter.java
+++ user-microservice/src/main/java/nl/tudelft/sem/template/user/domain/userlogic/converters/GenderConverter.java
@@ -1,2 +1,28 @@
-package nl.tudelft.sem.template.user.domain.userlogic.converters;public class GenderConverter {
+package nl.tudelft.sem.template.user.domain.userlogic.converters;
+
+import nl.tudelft.sem.template.user.domain.userlogic.Gender;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.util.Arrays;
+
+@Converter
+public class GenderConverter implements AttributeConverter<Gender, String> {
+
+    @Override
+    public String convertToDatabaseColumn(Gender type) {
+        return type.toString();
+    }
+
+    @Override
+    public Gender convertToEntityAttribute(String dbData) throws IllegalArgumentException {
+        if (dbData == null) {
+            throw new IllegalArgumentException();
+        }
+
+        return Arrays.stream(Gender.values())
+                .filter(v -> v.getType().equals(dbData))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
 }
