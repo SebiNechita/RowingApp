1481e411cb690e69b3c7710a06b2621e75149e8c
Iannis de Zwart <I.R.E.deZwart@student.tudelft.nl>
2022-12-17 14:15:58 +0100

    Fix response error relay

--- common/src/main/java/nl/tudelft/sem/template/common/http/HttpUtils.java
+++ common/src/main/java/nl/tudelft/sem/template/common/http/HttpUtils.java
@@ -1,29 +1,38 @@
 package nl.tudelft.sem.template.common.http;
 
 import org.springframework.http.*;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
 
 public class HttpUtils {
 
     public static <RequestBodyType, ResponseBodyType> ResponseEntity<ResponseBodyType>
     sendHttpRequest(String url, HttpMethod method, RequestBodyType body, Map<String, String> headers,
-                    Class<ResponseBodyType> responseBodyType) {
+                    Class<ResponseBodyType> responseBodyType) throws ResponseStatusException {
         var restTemplate = new RestTemplate();
         var reqHeaders = new HttpHeaders();
         reqHeaders.setContentType(MediaType.APPLICATION_JSON);
 
-        for (Map.Entry<String, String> header : headers.entrySet()) {
+        for (var header : headers.entrySet()) {
             reqHeaders.set(header.getKey(), header.getValue());
         }
 
-        return restTemplate.exchange(url, method, new HttpEntity<>(body), responseBodyType);
+        try {
+            return restTemplate.exchange(url, method, new HttpEntity<>(body, reqHeaders), responseBodyType);
+        } catch (HttpStatusCodeException e) {
+            throw new ResponseStatusException(e.getStatusCode(), e.getResponseBodyAsString());
+        }
     }
 
     public static <RequestBodyType, ResponseBodyType> ResponseEntity<ResponseBodyType>
-    sendHttpRequest(String url, HttpMethod method, RequestBodyType body, Class<ResponseBodyType> responseBodyType) {
+    sendHttpRequest(String url, HttpMethod method, RequestBodyType body,
+            Class<ResponseBodyType> responseBodyType) throws ResponseStatusException {
         return sendHttpRequest(url, method, body, new HashMap<>(), responseBodyType);
     }
 }
