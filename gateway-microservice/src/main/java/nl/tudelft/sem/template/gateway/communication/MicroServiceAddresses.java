7f2fd2fe4b83895aac9928b3232c721afe70395e
Iannis de Zwart <i.r.e.dezwart@student.tudelft.nl>
2022-12-20 12:12:32 +0100

    Remove proxyman DNS

--- gateway-microservice/src/main/java/nl/tudelft/sem/template/gateway/communication/MicroServiceAddresses.java
+++ gateway-microservice/src/main/java/nl/tudelft/sem/template/gateway/communication/MicroServiceAddresses.java
@@ -1,9 +1,11 @@
 package nl.tudelft.sem.template.gateway.communication;
 
 public class MicroServiceAddresses {
-    public static final String authenticationMicroservice = "http://localhost.proxyman.io:8081";
-    public static final String rowingInfoMicroservice = "http://localhost.proxyman.io:8088";
-    public static final String activityOfferMicroservice = "http://localhost.proxyman.io:8083";
-    public static final String activityMatchMicroservice = "http://localhost.proxyman.io:8086";
-    public static final String userMicroservice = "http://localhost.proxyman.io:8085";
+    public static final String hostname = "http://localhost";
+
+    public static final String authenticationMicroservice = hostname + ":8081";
+    public static final String rowingInfoMicroservice     = hostname + ":8088";
+    public static final String activityOfferMicroservice  = hostname + ":8083";
+    public static final String activityMatchMicroservice  = hostname + ":8086";
+    public static final String userMicroservice           = hostname + ":8085";
 }
