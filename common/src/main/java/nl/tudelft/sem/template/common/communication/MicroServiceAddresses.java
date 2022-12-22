d539cc05d7e1428cd45402ebd17a5c1f71a57773
Sebastian Nechita <89786261+SebiNechita@users.noreply.github.com>
2022-12-22 11:25:17 +0200

    Change one of the localhost

--- common/src/main/java/nl/tudelft/sem/template/common/communication/MicroServiceAddresses.java
+++ common/src/main/java/nl/tudelft/sem/template/common/communication/MicroServiceAddresses.java
@@ -2,7 +2,7 @@ package nl.tudelft.sem.template.common.communication;
 
 public class MicroServiceAddresses {
     public static final String authenticationMicroservice = "http://localhost:8081";
-    public static final String rowingInfoMicroservice = "http://localhost:8082";
+    public static final String rowingInfoMicroservice = "http://localhost:8088";
     public static final String activityOfferMicroservice = "http://localhost:8083";
     public static final String activityMatchMicroservice = "http://localhost:8084";
     public static final String userMicroservice = "http://localhost:8085";
