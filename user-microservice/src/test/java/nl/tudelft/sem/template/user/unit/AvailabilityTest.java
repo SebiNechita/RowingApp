fe733e60eb348f7c19273f8c70ed22a1b284a664
Sebastian Nechita <89786261+SebiNechita@users.noreply.github.com>
2023-01-24 11:58:35 +0100

    Kill mutants on Availability class
    Increased the number of mutants killed and also the line coverage.

--- user-microservice/src/test/java/nl/tudelft/sem/template/user/unit/AvailabilityTest.java
+++ user-microservice/src/test/java/nl/tudelft/sem/template/user/unit/AvailabilityTest.java
@@ -8,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.TreeMap;
+import nl.tudelft.sem.template.user.domain.userlogic.NetId;
 import nl.tudelft.sem.template.user.domain.userlogic.Tuple;
 import nl.tudelft.sem.template.user.domain.userlogic.entities.Availability;
 import nl.tudelft.sem.template.user.domain.userlogic.exceptions.AvailabilityOverlapException;
@@ -69,5 +70,32 @@ class AvailabilityTest {
         treeMap.put(dateOneIntervalOne, dateTwoIntervalOne);
         treeMap.put(dateOneIntervalTwo, dateTwoIntervalTwo);
         assertFalse(Availability.overlap(treeMap));
+    }
+
+    @Test
+    void checkEquals() {
+        LocalDateTime dateOneIntervalOne = LocalDateTime.parse("2022-12-12T13:30");
+        LocalDateTime dateTwoIntervalOne = LocalDateTime.parse("2022-12-12T15:00");
+        LocalDateTime dateOneIntervalTwo = LocalDateTime.parse("2022-12-31T13:40");
+        LocalDateTime dateTwoIntervalTwo = LocalDateTime.parse("2022-12-31T22:00");
+        NetId netId = new NetId("test");
+        NetId netIdTwo = new NetId("test2");
+        Availability availability = new Availability(netId, dateOneIntervalOne, dateTwoIntervalOne);
+        Availability availability2 = new Availability(netId, dateOneIntervalOne, dateTwoIntervalOne);
+        Availability availability3 = new Availability(netIdTwo, dateOneIntervalTwo, dateTwoIntervalTwo);
+        System.out.println(availability.getId() + " " + availability2.getId() + " " + availability3.getId());
+        assertTrue(availability.equals(availability2));
+        assertTrue(availability.equals(availability));
+        assertFalse(availability.equals(null));
+        assertFalse(availability.equals(netIdTwo));
+    }
+
+    @Test
+    void checktoString() {
+        LocalDateTime dateOneIntervalOne = LocalDateTime.parse("2022-12-12T13:30");
+        LocalDateTime dateTwoIntervalOne = LocalDateTime.parse("2022-12-12T15:00");
+        NetId netId = new NetId("test");
+        Availability availability = new Availability(netId, dateOneIntervalOne, dateTwoIntervalOne);
+        assertEquals("2022-12-12T13:30,2022-12-12T15:00", availability.toString());
     }
 }
